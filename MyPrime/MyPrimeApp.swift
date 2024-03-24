db25c52485254d087d3229575d645def03ff35d6
Amit Patel <amitpatel28092001@gmail.com>
2024-03-24 16:12:26 +0530

    Initial Commit

---
+++ MyPrime/MyPrimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPrimeApp.swift
+//  MyPrime
+//
+//  Created by Amit Patel on 24/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MyPrimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
